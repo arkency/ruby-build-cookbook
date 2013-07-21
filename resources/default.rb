1d6ce8ab55adb94425b83179703702c969e60d2c
Paweł Pacana <pawel.pacana@gmail.com>
2013-07-21 21:29:42 +0200

    Fix typo.

--- resources/default.rb
+++ resources/default.rb
@@ -8,6 +8,6 @@ end
 attribute :export_path, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :version,     :kind_of => String
 attribute :home,        :kind_of => String
-attrubute :owner,       :kind_of => String
+attribute :owner,       :kind_of => String
 attribute :rubygems,    :kind_of => String
 attribute :exports,     :kind_of => Array
