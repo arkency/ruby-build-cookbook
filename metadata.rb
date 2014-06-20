cf8f5a0202f4cabfd7501da85ef87ad44f6d03bb
Paweł Pacana <pawel.pacana@gmail.com>
2014-06-20 11:25:42 +0200

    Add missing name metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "ruby-build"
 maintainer       "Arkency"
 maintainer_email "dev@arkency.com"
 license          "Apache 2.0"
 description      "Installs/Configures user ruby by using ruby-build"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.7"
+version          "0.3.8"
